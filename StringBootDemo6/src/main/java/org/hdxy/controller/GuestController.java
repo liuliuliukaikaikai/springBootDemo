e146f448286baa6e2b67715de658542d99a21fa1
GiveItYourAll <1109259152@qq.com>
2020-11-22 15:55:34 +0800

    传统方式 改成 Rest风格

--- StringBootDemo6/src/main/java/org/hdxy/controller/GuestController.java
+++ StringBootDemo6/src/main/java/org/hdxy/controller/GuestController.java
@@ -5,19 +5,20 @@ import org.hdxy.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/guest")
 public class GuestController {
     @Autowired
     private GuestService guestService;
 
 
     //返回嘉宾列表页
-    @RequestMapping("/guest/list")
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String list(Model model) {
         List<Guest> list = guestService.list();
         model.addAttribute("guestList", list);
@@ -25,36 +26,35 @@ public class GuestController {
     }
 
     //跳转增加嘉宾页面
-    @RequestMapping("/guest/toadd")
+    @GetMapping("/toadd")
     public String toadd() {
         return "add";
     }
-
     //增加嘉宾
-    @RequestMapping("/guest/add")
+    @PostMapping
     public String add(Guest guest) {
         guestService.add(guest);
-        return "redirect:/guest/list";
+        return "redirect:/guest";
     }
 
     //跳转修改嘉宾页面
-    @RequestMapping("/guest/toupdate")
-    public String toupdate(Model model, String name) {
+    @GetMapping("/toupdate/{name}")
+    public String toupdate(Model model,@PathVariable("name") String name) {
         Guest guest = guestService.get(name);
         model.addAttribute(guest);
         return "update";
     }
 
     //修改嘉宾
-    @RequestMapping("/guest/update")
+    @PutMapping
     public String update(Guest guest) {
         guestService.update(guest);
-        return "redirect:/guest/list";
+        return "redirect:/guest";
     }
     //删除嘉宾
-    @RequestMapping("/guest/delete")
-    public String delete(String name) {
+    @DeleteMapping("/{name}")
+    public String delete(@PathVariable("name") String name) {
         guestService.delete(name);
-        return "redirect:/guest/list";
+        return "redirect:/guest";
     }
 }
