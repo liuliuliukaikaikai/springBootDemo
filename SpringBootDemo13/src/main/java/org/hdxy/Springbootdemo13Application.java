79c2de6baacc8cb948b6e123ecb0617ba58cfdbc
GiveItYourAll <1109259152@qq.com>
2020-12-12 09:26:30 +0800

    最后一次提交

---
+++ SpringBootDemo13/src/main/java/org/hdxy/Springbootdemo13Application.java
@@ -0,0 +1,13 @@
+package org.hdxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootdemo13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootdemo13Application.class, args);
+    }
+
+}
